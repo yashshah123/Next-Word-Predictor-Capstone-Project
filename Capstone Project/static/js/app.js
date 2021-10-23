b624d3c9cf67ae3a24051065d1aabdec302e1a2a
yashshah123 <shahyash095@gmail.com>
2021-10-23 13:28:09 -0400

    Add files via upload

---
+++ Capstone Project/static/js/app.js
@@ -0,0 +1,36 @@
+var data = []
+var token = ""
+
+jQuery(document).ready(function () {
+    var slider = $('#max_words')
+    slider.on('change mousemove', function (evt) {
+        $('#label_max_words').text('Top k words: ' + slider.val())
+    })
+
+    $('#input_text').on('keyup', function (e) {
+        if (e.key == ' ') {
+            $.ajax({
+                url: '/get_end_predictions',
+                type: "post",
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify({
+                    "input_text": $('#input_text').val(),
+                    "top_k": slider.val(),
+                }),
+                beforeSend: function () {
+                    $('.overlay').show()
+                },
+                complete: function () {
+                    $('.overlay').hide()
+                }
+            }).done(function (jsondata, textStatus, jqXHR) {
+                console.log(jsondata)
+                $('#text_bert').val(jsondata['bert'])
+            }).fail(function (jsondata, textStatus, jqXHR) {
+                console.log(jsondata)
+            });
+        }
+    })
+
+})
